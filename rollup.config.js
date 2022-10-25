f2671598fff97bc4b70aace9f2a47671fe77d7d9
Vasileios Zalimidis <vasileios.zalimidis@external.engie.com>
2022-10-25 12:47:17 +0200

    package ready

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
-import typescript from "rollup-plugin-ts"
-import {lezer} from "@lezer/generator/rollup"
+import typescript from 'rollup-plugin-ts'
+import { lezer } from '@lezer/generator/rollup'
+import pkg from './package.json'
 
 export default {
-  input: "src/index.ts",
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
+  input: 'src/index.ts',
+  external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
   output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
+    { name: 'scil-language-package', file: pkg.browser, format: 'umd' },
+    { file: pkg.module, format: 'es' },
+    { file: 'dist/index.cjs', format: 'cjs' },
+    { dir: './dist', format: 'es' },
   ],
-  plugins: [lezer(), typescript()]
+  plugins: [lezer(), typescript()],
 }
